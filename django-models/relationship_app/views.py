9373426953cbe7421da1aebb9ec651979abbe9ff
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 17:32:18 +0100

    new

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -9,41 +9,6 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from .models import UserProfile
 
-def list_books(request):
-    books = Book.objects.all()  # Query to get all books
-    context = {
-        'books': books,
-    }
-    return render(request, 'relationship_app/list_books.html', context)
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-    def get_object(self):
-        return Library.objects.get(name = "Cairo")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        library = self.get_object()
-        books = library.books.all()
-        context['books'] = books
-        return context
-    
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Automatically log in the user after registration
-            return redirect(reverse_lazy('login'))  # Redirect to the login page
-    else:
-        form = UserCreationForm()
-    
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-# views.py
 
 def is_admin(user):
     return user.userprofile.role == 'Admin'
