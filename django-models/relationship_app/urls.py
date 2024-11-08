f985523c770f6fc662614640d8d66b9f47359dba
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 17:45:50 +0100

    ne

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, index
-from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+
 urlpatterns = [
-    path("list_books/", list_books, name="list_books"),
-    path("library_detail/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", views.register, name="register"),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
