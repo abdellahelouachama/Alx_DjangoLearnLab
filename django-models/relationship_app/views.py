d8478d1edd824213d86eba383d23ec3baa4e432e
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 17:40:25 +0100

    new

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render, redirect
-from .models import Library, Book
-from django.views.generic.detail import DetailView
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
-from .models import UserProfile
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
 
+def role_required(role):
+    def decorator(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return user_passes_test(decorator)
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
+@role_required('Admin')
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, 'admin_view.html')
+
+@role_required('Librarian')
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@role_required('Member')
+def member_view(request):
+    return render(request, 'member_view.html')
