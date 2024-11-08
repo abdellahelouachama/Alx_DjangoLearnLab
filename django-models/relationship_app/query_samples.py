736463a9c9523d260e0c15ae58ef01952c47721b
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 15:43:42 +0100

    new
    git commit -mnew

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -2,4 +2,5 @@ from models import Author, Book, Library, Librarian
 
 books = Book.objects.select_related('author').get(id=1)
 Library.objects.get(name='library_name')
+books.all()
 Librarian.objects.select_related('library').all()
