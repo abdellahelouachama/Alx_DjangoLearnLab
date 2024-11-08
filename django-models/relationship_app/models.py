b06ebd84ff3bbe6184219d3006ab63e1065c39ef
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 17:20:29 +0100

    new

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,72 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
-
-# Create your models here.
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
-    
-    def __str__(self):
-        return f"{self.title} by {self.author}"  
-
-#Extending Book Model with Custom Permissions
-    class Meta(models.Model):
-        Permissions_Choices =(
-            ('can_add_book', 'can_add_book'),
-            ('can_change_book', 'can_change_book'),
-            ('can_delete_book', 'can_delete_book'),
-
-        )
-    permissions = models.CharField(max_length=50,  choices='Permissions_Choices')
-    meta = models.TextField()
-    
-    def __str__(self):
-        return f'{self.user.username} - {self.permissions}'
-
-class Library(models.Model):
-    name = models.CharField(max_length=200)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name   
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=200)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarians')
-
-    def __str__(self):
-        return self.name
-    
-#Extending User Model with a UserProfile
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    Role_Choices =(
-        ('Admin','Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    
-    )
-
-role = models.CharField(max_length=50,  choices='Role_Choices')
-userprofile = models.TextField()
-
-def __str__(self):
-    return f'{self.user.username} - {self.role}'
-
-#Signal to automatically create a UserProfile when a new User is created
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# Extending User Model with a UserProfile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+    
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
 
+    def __str__(self):
+        return f'{self.user.username} - {self.role}'
+
+# Signal to automatically create a UserProfile when a new User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -75,3 +27,24 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+# Book Model with Custom Permissions
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE, related_name='author')
+    
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
