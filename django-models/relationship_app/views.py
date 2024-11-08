f4e64d95583ad7570056c8c9c1e564447b2ebabd
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 16:09:23 +0100

    new
    git commit -mnew

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from . models import Author, Book, Library, Librarian
 # Create your views here.
 def list_books(request):
-    books = Book.objects.select_related("author").all()
+    books = Book.objects.all()
     context = {
         "books": books
     }
