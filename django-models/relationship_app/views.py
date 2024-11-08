f8d8eb29016abc5b35db66596b7bd14ed942f5eb
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-08 17:11:06 +0100

    new

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -59,7 +59,7 @@ def index(request):
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-# @login_required
+@login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
