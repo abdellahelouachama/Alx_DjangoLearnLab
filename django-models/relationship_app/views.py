3007cce77c2e0353d7b254d2ef6dd6bc623a656c
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 15:39:58 +0100

    try more

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -45,7 +45,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 def is_admin(request):
-    return request.user.UserProfile.role == 'Admin'
+    return request.user.UserProfile.filter (role='Admin')
 
 # @login_required(login_url='login')
 @user_passes_test(is_admin)
