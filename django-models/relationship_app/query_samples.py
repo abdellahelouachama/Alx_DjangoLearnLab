df3863b97190c421055b5898bb86723fca2dd3e3
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 12:11:32 +0100

    try more1

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -6,3 +6,4 @@ from models import Author, Book, Library, Librarian
 Library.objects.get(name=library_name).books.all()
 Author.objects.get(name=author_name).objects.filter(author=author)
 Librarian.objects.get(library = library_name)
+Librarian.objects.get(library=
