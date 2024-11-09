aad1123317c53760b9c67a9094b96bf3550d6d21
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 12:39:41 +0100

    try more1

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from . models import Book
 from .models import Library
 # Create your views here
@@ -10,7 +10,7 @@ def list_books_view(request):
     }
     return render(request, 'relationship_app/list_books.html', context)
 
-class LibraryDetailView(ListView):
+class LibraryDetailView(DetailView):
     model = Library
 
     def specific_library(self, request, library_name):
