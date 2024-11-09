d199c086d1e04af4c26691833b900eb86c8fb4cf
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 13:19:34 +0100

    try more1

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import list_books
+# from .views import list_books, LibraryDetailView
+
+# urlpatterns = [
+#     path('book list', list_books, name='list_books'),
+#     path('library detail <int:library_id>', LibraryDetailView, name='library_detail'),
+   
+# ]
+from . import views
 
 urlpatterns = [
-    path('book list',list_books, name='list_books'),
-    path('library detail <int:library_id>', LibraryDetailView, name='library_detail'),
-   
+    path('register', views.register, name='register'),
+    path('login', views.CustomLoginView.as_view(), name='login'),
+    path('logout', views.CustomLogoutView.as_view(), name='logout'),
+
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,21 +1,41 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from . models import Book
-from .models import Library
-# Create your views here
-def list_books(request):
-    books = Book.objects.all()
-    context = {
-        'books' : books
-    }
-    return render(request, 'relationship_app/list_books.html', context)
+from django.shortcuts import render, redirect
+# from django.views.generic.detail import DetailView
+# from . models import Book, Library
+ 
+# # Create your views here
+# def list_books(request):
+#     books = Book.objects.all()
+#     context = {
+#         'books' : books
+#     }
+#     return render(request, 'relationship_app/list_books.html', context)
 
-class LibraryDetailView(DetailView):
-    model = Library
+# class LibraryDetailView(DetailView):
+#     model = Library
 
-    def specific_library(self, request, library_name):
-        library = Library.objects.filter(name=library_name)
-        context = {
-            'library' : library
-        }
-        return render(request, 'relationship_app/library_detail.html', context)
+#     def specific_library(self, request, library_name):
+#         library = Library.objects.filter(name=library_name)
+#         context = {
+#             'library' : library
+#         }
+#         return render(request, 'relationship_app/library_detail.html', context)
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy    
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+class CustomLogoutView(LogoutView):
+    template_name = 'registration/logout.html'   
+    Seccess_url = reverse_lazy('login') 
