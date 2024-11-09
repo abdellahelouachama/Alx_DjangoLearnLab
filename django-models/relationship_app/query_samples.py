3dfac119fe19174a2bc1cc4ec860a56a66bbadd8
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 11:57:48 +0100

    try more

--- django-models/relationship_app/query_samples.py
+++
@@ -1,8 +0,0 @@
-from models import Author, Book, Library, Librarian
-
-books = Book.objects.select_related('author').get(id=1)
-Library.objects.get(name=library_name)
-books.all()
-Author.objects.get(name=author_name)
-objects.filter(author=author)
-Librarian.objects.get(library=library)
