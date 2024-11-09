42397e05b2bf4efedbe6aee665eac96dac9d4bde
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-09 15:23:19 +0100

    try more

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -49,7 +49,7 @@ def is_admin(request):
 
 @login_required(login_url='login')
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, 'admin_view.py')
 
 def is_librarian(request):
