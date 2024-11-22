4fd31ee3eeae3f665e8c7d25786452bb46d7d75e
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-22 16:26:57 +0100

    API development

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,9 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
+
 # Create your views here.
 
 class BookList(generics.ListAPIView):
