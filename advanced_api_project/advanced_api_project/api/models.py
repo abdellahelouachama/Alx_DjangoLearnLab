50313b1a4a4fe3ccdbe5513fd765141e8e55f0c5
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-29 18:10:06 +0100

    API development

---
+++ advanced_api_project/advanced_api_project/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+# Author and Book models are created with one to many relationship between them
