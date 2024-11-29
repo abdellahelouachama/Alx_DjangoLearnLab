7846c35097be68f5b837a73c0daa77340a438b2a
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-29 15:17:34 +0100

    API develompent

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Book
-
+from django.contrib.auth.models import User
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -7,8 +7,9 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 
+
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)),
-    path('api-token-auth/', obtain_auth_token),
+    path('token-auth/', obtain_auth_token),
 ]
