66f9391d09f1de9df5e018239ab212c03d780734
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-30 12:32:45 +0100

    advanced API

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,9 +6,9 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('login/', views.obtain_auth_token),
     path('books/', ListView.as_view()),
-    path('books/<int:pk>/', DeleteView.as_view()),
-    path('books/', CreateView.as_view()),
-    path('books/<int:pk>/', UpdateView.as_view()),
-    path('books/<int:pk>/', DeleteView.as_view())
+    path('books/<int:pk>/', DetailView.as_view()),
+    path('books/create/', CreateView.as_view()),
+    path('books/Update/<int:pk>/', UpdateView.as_view()),
+    path('books/delete/<int:pk>/', DeleteView.as_view())
 
 ]
