094d109847b5106082c4ef1d38ebc0698ab07f86
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-11-30 12:33:11 +0100

    advanced API

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('books/', ListView.as_view()),
     path('books/<int:pk>/', DetailView.as_view()),
     path('books/create/', CreateView.as_view()),
-    path('books/Update/<int:pk>/', UpdateView.as_view()),
+    path('books/Update<int:pk>/', UpdateView.as_view()),
     path('books/delete/<int:pk>/', DeleteView.as_view())
 
 ]
