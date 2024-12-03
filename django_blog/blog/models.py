46cefca63ad4e50c2951ebd77d77156af43cc869
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-12-03 14:30:19 +0100

    plog post Crud operations

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
