864123d5e4d8b3847100be63c861c41739df3922
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-12-12 16:45:39 +0100

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,10 @@ from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+# serializers.CharField()
+# serializers.CharField()
+# get_user_model().objects.create_user
+
 # serializer to handle user data conversion (serializing and deserializing)
 class UserSerializer(ModelSerializer):
     class Meta:
